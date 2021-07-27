52bf8ac3b071394ee8ee82aa501c5f5f6c4dd646
Ganesh <ganeshgavhad9@gmail.com>
2021-07-28 01:20:39 +0530

    Added : UC2 of user need to enter validl last name

--- com/bridgelabz/UserRegistrationRegex.java
+++ com/bridgelabz/UserRegistrationRegex.java
@@ -11,29 +11,47 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistrationRegex {
-	//user1 is object userDetails Class 
+	// user1 is object userDetails Class
 	static UserDetails user1 = new UserDetails();
 	static Scanner sc = new Scanner(System.in);
-	
+
 	/**
-	 * @getFirstNama is a method for validation user First name
-	 * by using regex pattern matches method .
+	 * @getFirstName is a method for validation user First name by using regex
+	 *               pattern matches method .
 	 */
-	public static void getFirstNama() {
+	public static void getFirstName() {
 		System.out.println("Enter First Name");
 		String firstName = sc.next();
 		boolean check = Pattern.matches("[A-Z]{1}+[a-z]{2,}", firstName);
-		if(check) {
-			user1.setFirstName(firstName);	// if check become true
-										   //user first name will get added in user1 object
-		}
-		else {							// else user get instruction to type first name as per pattern
+		if (check) {
+			user1.setFirstName(firstName); // if check become true
+											// user first name will get added in user1 object
+		} else {
+			// else user get instruction to type first name as per pattern
 			System.out.println("First name start with Cap and has minimum 3 characters");
 		}
 	}
 
+	/**
+	 * @getLastName is a method for validation user Last name by using regex
+	 * pattern matches method .
+	 */
+	public static void getLastName() {
+		System.out.println("Enter Last Name");
+		String lastName = sc.next();
+		boolean check = Pattern.matches("[A-Z]{1}+[a-z]{2,}", lastName);
+		if (check) {
+			user1.setFirstName(lastName); 	// if check become true
+											// user last name will get added in user1 object
+		} else { 
+			// else user get instruction to type last name as per pattern
+			System.out.println("Last name start with Cap and has minimum 3 characters");
+		}
+	}
+
 	public static void main(String[] args) {
-		getFirstNama();
+		getFirstName();
+		getLastName();
 	}
 
 }
